039847b07b00f466558a8d6155956ab9baa5b6b4
JulianLopez711 <lopezito02@gmail.com>
2025-04-02 11:04:35 -0500

    cambios a base de datos

---
+++ app/models/models.py
@@ -0,0 +1,9 @@
+from . import db
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Usuario {self.nombre}>'
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'clave_secreta')
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:110723.Jv@rentadb.cluster-ro-cn7bggvpiqfk.us-east-1.rds.amazonaws.com/rentadb"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@dbrenta.cn7bggvpiqfk.us-east-1.rds.amazonaws.com/dbrenta"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
